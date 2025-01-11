dac15ebf97ea2b7a09c61c039e1b91ba695c3764
ChehST <chehstream@gmail.com>
2025-01-11 20:50:46 +0400

    Brought out start command to separate file

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,4 +1,7 @@
 from .request import command_request
 from .avaliable import command_avaliable
 from .qa import command_qa
-__ALL__ = ['command_request', 'command_avaliable','qa']
+from .start import command_start
+
+__ALL__ = ['command_request', 'command_avaliable','qa',
+           'command_start']
